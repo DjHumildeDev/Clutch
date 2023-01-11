b643a10ae22a689dfaf1cf12978e69ce56b9fc8d
rudeboinas <choches61@gmail.com>
2023-01-11 12:57:16 +0100

    mantenimiento incidencias

---
+++ Clutch/Clutch/Clutch/Views/MantenimientoIncidencias.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clutch.Views
+{
+    /// <summary>
+    /// Lógica de interacción para MantenimientoIncidencias.xaml
+    /// </summary>
+    public partial class MantenimientoIncidencias : Window
+    {
+        public MantenimientoIncidencias()
+        {
+            InitializeComponent();
+        }
+
+        private void dtPckFecha_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void chBxResuelta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void cmBxTipo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmBxTipo_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmBxEmpleado_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void mnIncidenciasBorrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnIncidenciassEditar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnIncidenciasCrear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnIncidenciasSalir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void lvJornadas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
