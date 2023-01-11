e03803891248653223fa443b8b52b7935e82a5c3
rudeboinas <choches61@gmail.com>
2023-01-11 12:21:34 +0100

    vista Jornadas

---
+++ Clutch/Clutch/Clutch/Views/MantenimientoJornadas.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clutch.Views
+{
+    /// <summary>
+    /// Lógica de interacción para MantenimientoJornadas.xaml
+    /// </summary>
+    public partial class MantenimientoJornadas : Window
+    {
+        public MantenimientoJornadas()
+        {
+            InitializeComponent();
+        }
+
+        private void lvJornadas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void mnJornadasCrear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnJornadasEditar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnJornadasBorrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void mnJornadasSalir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dtPckSalida_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmBxEmpleado_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dtPckEntrada_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
