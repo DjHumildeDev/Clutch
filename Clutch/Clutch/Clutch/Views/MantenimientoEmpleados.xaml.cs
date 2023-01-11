3be967215660c68aded66ef569363d34d6410481
rudeboinas <choches61@gmail.com>
2023-01-11 12:39:24 +0100

    view mantenimeinto empleado

---
+++ Clutch/Clutch/Clutch/Views/MantenimientoEmpleados.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clutch.Views
+{
+    /// <summary>
+    /// Lógica de interacción para MantenimientoEmpleados.xaml
+    /// </summary>
+    public partial class MantenimientoEmpleados : Window
+    {
+        public MantenimientoEmpleados()
+        {
+            InitializeComponent();
+        }
+    }
+}
