21c0756b7c48e5673596b566cd26e6b4b05cb9b4
rudeboinas <choches61@gmail.com>
2023-01-14 16:53:20 +0100

    NUEVAS VIEWS

---
+++ Clutch/Clutch/Clutch/Enums/CentimetrosCubicosEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clutch.Enums
+{
+    public enum CentimetrosCubicosEnum
+    {
+        CC49,
+        CC125
+    }
+}
---
+++ Clutch/Clutch/Clutch/Enums/EstadoMotoEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clutch.Enums
+{
+    public enum EstadoMotoEnum
+    {
+        Disponible,
+        Ocupada,
+        En_Taller,
+        Averiada
+    }
+}
