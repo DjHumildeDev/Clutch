bdb62db265b12ee3a44cb55c5a4ef00531c93ebc
rudeboinas <choches61@gmail.com>
2023-02-01 15:05:20 +0100

    Logica mantenimiento motos

--- Clutch/Clutch/Clutch/Views/MantenimientoMotos.xaml.cs
+++ Clutch/Clutch/Clutch/Views/MantenimientoMotos.xaml.cs
@@ -56,13 +56,51 @@ namespace Clutch.Views
 
         private void mnMotosEditar_Click(object sender, RoutedEventArgs e)
         {
-             
+            if (lvMotos.SelectedItems.Count == 1)
+            {
+                Moto ver = (Moto)((ListViewItem)lvMotos.SelectedItems[0]).Tag;
+                VerMoto ventana = new VerMoto(ver);
+                ventana.Owner = this;
+                if (ventana.ShowDialog() == true)
+                {
+                    negocio.EditarMoto(ver);
+                    ActualizarLista();
+                }
+                else
+                {
+                    MessageBox.Show("Necesitas seleccionar un elemento", "No hay seleccion", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            } 
 
         }
 
         private void mnMotosBorrar_Click(object sender, RoutedEventArgs e)
         {
+            if (lvMotos.SelectedItems.Count == 1)
+            {
+                //Proceso de identificacion
 
+                MessageBoxResult result = MessageBox.Show("¿Esta seguro que quiere borrar esta  Jornada?", "¡Atencion!", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.OK)
+                {
+                    Moto borrar = (Moto)((ListViewItem)lvMotos.SelectedItems[0]).Tag;
+
+                    if (negocio.BorrarMoto(borrar))
+                    {
+                        MessageBox.Show("Se ha borrado la jornada correctamente", "Jornada Borrada", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al eliminar la jornada", "Algo salio mal", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    ActualizarLista();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Necesitas seleccionar un elemento", "No hay seleccion", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
 
         private void mnMotoAsignar_Click(object sender, RoutedEventArgs e)
